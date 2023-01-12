1e1c8715c2788e3f6e41830919df081968e5e211
Svetlana Georgieva <sv.georgieva@pgzaimov.com>
2023-01-12 09:42:19 +0200

    Add project files.

---
+++ Kristian_kursova_majev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kristian_kursova_majev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
